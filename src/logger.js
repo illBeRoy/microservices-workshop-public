f409f51bf4d47579511fdab305a3bbf17f6221d8
Roy Sommer <royso@wix.com>
2018-05-07 09:48:23 +0300

    updated logger

--- src/logger.js
+++ src/logger.js
@@ -1,7 +1,7 @@
 export const logger = () =>
   (req, res, next) => {
     if (process.env.NODE_ENV !== 'test') {
-      console.log(`[${req.method.toUpperCase()}] ${req.path} ${JSON.stringify(req.query || req.body)}`);
+      console.log(`[${req.method.toUpperCase()}] ${req.path} ${JSON.stringify(req.query)} ${JSON.stringify(req.body)}`);
     }
 
     next();
